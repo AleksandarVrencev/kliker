21a72ec945f44ebb6b4e8dab2f6ac1606dec822f
Aleksandar Vrencev <aleksandarvrencev@gmail.com>
2022-11-20 13:36:11 +0100

    finished app

--- kliker.py
+++ kliker.py
@@ -3,6 +3,7 @@ import pyautogui
 import os
 import tkinter as tk
 from pynput import keyboard
+from tkinter import *
 from tkinter import messagebox, filedialog, simpledialog
 # row counter
 i = 1
@@ -12,13 +13,14 @@ lista = []
 key_presses = []
 # bool to stop recording click binds as L,R,D to be able to record key presses
 bool_click_recording = True
+entry_var = ""
 def main():
     # make a tkinter window with a button 
     root = tk.Tk()
     root.title('Kliker')
-    root.geometry('450x600')
+    root.geometry('500x500')
     root.resizable(True, True)
-
+    hotkey = tk.StringVar(root)
     # make a function that will click on all positions in the list
     def start():
         if listbox.size() == 0:
@@ -34,9 +36,10 @@ def main():
                 time.sleep(1)
             # maximize window
             root.deiconify() 
-    #
+
+    # make a function that will clear the list
     def clear():
-        global i
+        global i, entry_var
         # reset row counter
         i = 1
         # clear list
@@ -44,50 +47,36 @@ def main():
         # clear listbox
         listbox.delete(0, tk.END)
         # clear entry boxes
-        entry_var.set('')
-    #
+        entry_var = ""
+    
+    # make a function that will record keyboard input
+    keys_list = ['ctrl', 'alt', 'shift', 'win']
     def record_keyboard_input():
-        global bool_click_recording, i
-        #bool_click_recording = False
-        if hotkey.get() == 'ctrl':
-            listbox.insert(tk.END, str(i) + "." + str(pyautogui.position().x) + " " + str(pyautogui.position().y) + " " + 'ctrl' + ' ' + entry_var.get())
-            i += 1
-            lista.append([pyautogui.position().x, pyautogui.position().y, 'ctrl' , entry_var.get()])
-            bool_click_recording = True
-            entry_var.set('')
-            root.focus()
-        elif hotkey.get() == 'alt':
-            listbox.insert(tk.END, str(i) + "." + str(pyautogui.position().x) + " " + str(pyautogui.position().y) + " " + 'alt' + ' ' + entry_var.get())
-            i += 1
-            lista.append([pyautogui.position().x, pyautogui.position().y, 'alt' , entry_var.get()])
-            bool_click_recording = True
-            entry_var.set('')
-            root.focus()
-        elif hotkey.get() == 'shift':
-            listbox.insert(tk.END, str(i) + "." + str(pyautogui.position().x) + " " + str(pyautogui.position().y) + " " + 'shift' + ' ' + entry_var.get())
-            i += 1
-            lista.append([pyautogui.position().x, pyautogui.position().y, 'shift' , entry_var.get()])
-            bool_click_recording = True
-            entry_var.set('')
-            root.focus()
-        elif hotkey.get() == 'win':
-            listbox.insert(tk.END, str(i) + "." + str(pyautogui.position().x) + " " + str(pyautogui.position().y) + " " + 'win' + ' ' + entry_var.get())
-            i += 1
-            lista.append([pyautogui.position().x, pyautogui.position().y, 'win' , entry_var.get()])
-            bool_click_recording = True
-            entry_var.set('')
-            root.focus()
+        global bool_click_recording, i, entry_var
+        if entry_var == "":
+            messagebox.showinfo('Kliker', 'Enter text!')
         else:
-            listbox.insert(tk.END, str(i) + "." + str(pyautogui.position().x) + " " + str(pyautogui.position().y) + " " + 'key' + ' ' + entry_var.get())
-            i += 1
-            lista.append([pyautogui.position().x, pyautogui.position().y, 'key' , entry_var.get()])
-            bool_click_recording = True
-            entry_var.set('')
-            root.focus()
-
+            if hotkey.get() in keys_list:
+                listbox.insert(tk.END, str(i) + "." + str(pyautogui.position().x) + " " + str(pyautogui.position().y) + " " + hotkey.get() + ' ' + entry_var)
+                i += 1
+                lista.append([pyautogui.position().x, pyautogui.position().y, hotkey.get() , entry_var])
+                bool_click_recording = True
+                entry_var = ""
+                root.focus()
+            else:
+                listbox.insert(tk.END, str(i) + "." + str(pyautogui.position().x) + " " + str(pyautogui.position().y) + " " + 'key' + ' ' + entry_var)
+                i += 1
+                lista.append([pyautogui.position().x, pyautogui.position().y, 'key' , entry_var])
+                bool_click_recording = True
+                entry_var = ""
+                root.focus()
+        hotkey.set('Hotkeys')
+    click_type_list = ['ctrl', 'alt', 'shift', 'win']
     # send mouse position and click type to a function that will click on that position
     def binder(mouse_x=0, mouse_y=0, click_type=None, key=None):
-        if(click_type == 'left'):
+        if click_type in click_type_list:
+            pyautogui.hotkey(click_type, key)
+        elif(click_type == 'left'):
             pyautogui.click(x=mouse_x, y=mouse_y, button='left')
         elif(click_type == 'right'):
             pyautogui.click(x=mouse_x, y=mouse_y, button='right')
@@ -95,14 +84,6 @@ def main():
             pyautogui.doubleClick(x=mouse_x, y=mouse_y, button='left')
         elif(click_type == 'key'):
             pyautogui.typewrite(key)
-        elif(click_type == 'ctrl'):
-            pyautogui.hotkey('ctrl', key)
-        elif(click_type == 'alt'):
-            pyautogui.hotkey('alt', key)
-        elif(click_type == 'shift'):
-            pyautogui.hotkey('shift', key)
-        elif(click_type == 'win'):
-            pyautogui.hotkey('win', key)
 
     # write save function that will save list to a file
     def save():
@@ -156,10 +137,6 @@ def main():
                     else:
                         listbox.insert(tk.END, str(j) + "." + line[0] + " " + line[1] + " " + line[2])
                         lista.append([int(line[0]), int(line[1]), line[2].strip()])
-                        #print(line)
-                    # append line to list
-                    # lista.append([int(line[0]), int(line[1]), line[2].strip()])
-                    # listbox.insert(tk.END, str(j) + '.' + str(line[0]) + ',' + str(line[1]) + ',' + str(line[2]))
                     j += 1 
                 # write success messageaca
                 messagebox.showinfo("Load", "File loaded successfully!")
@@ -168,7 +145,7 @@ def main():
                 i = j
         except:
             messagebox.showinfo("Warning", "You didn't select a file!")
-    #aca
+    # functions that bind the L,R and D buttons to mouse recording function
     def L(self):
         global i, bool_click_recording
         if bool_click_recording:
@@ -215,48 +192,64 @@ def main():
             lista.insert(index, [int(x[0]), int(x[1]), x[2], x[3]])
         else:
             lista.insert(index, [int(x[0]), int(x[1]), x[2]])
-        #lista.insert(index, [int(x[0]), int(x[1]), x[2]])
         # increment i
         i += 1
 
-    def bool_falser(self):
-        global bool_click_recording
-        bool_click_recording = False
+    # write function to take text input from user
+    def printer():
+        global entry_var
+        entry_var = simpledialog.askstring('Text', 'Enter text: ')
 
-    # add button to start the script
-    start_button = tk.Button(root, text='Start', command=start)
-    start_button.grid(row=0, column=0, sticky='w', ipadx=40.5, padx=10, pady=10)
-    # add button to clear the list
-    clear_button = tk.Button(root, text='Clear', command=clear)
-    clear_button.grid(row=2, column=1, sticky='e', ipadx=39.5)
-    # add entry field 
-    entry_var = tk.StringVar()
-    entry = tk.Entry(root, width=20,textvariable=entry_var)
-    entry.grid(row=1, column=1, sticky='e', padx=1, pady=10, ipadx=10)
-    # bind entry field to boolean variable in order to stop recording binding L,R,D to their functions
-    entry.bind('<FocusIn>', bool_falser)
-
-    # add hotkey dropdown menu to select hotkey like ctrl, alt, shift, etc.
-    hotkey = tk.StringVar(root)
-    #hotkey.set('ctrl')
-    hotkey_menu = tk.OptionMenu(root, hotkey, 'ctrl', 'alt', 'shift', 'win')
-    hotkey_menu.grid(row=0, column=1, sticky='w', padx=120)
-    # add button for record_keyboard_input() function
-    keyboard_input_button = tk.Button(root, text='Keyboard input', command=record_keyboard_input)
-    keyboard_input_button.grid(row=0, column=1, sticky='e')
-    # add button to save list to a file
-    save_button = tk.Button(root, text='Save', command=save)
-    save_button.grid(row=1, column=0, sticky='w', ipadx=40.5, padx=10, pady=10)
-    # add button to load list from a file
-    load_button = tk.Button(root, text='Load', command=load)
-    load_button.grid(row=2, column=0, sticky='w', ipadx=40, padx=10)
+    # add delete function to delete item from listbox 
+    def delete():
+        if listbox.curselection():
+            # get index of selected item
+            index = listbox.curselection()[0]
+            # delete selected item
+            listbox.delete(index)
+            # delete selected item from list
+            lista.pop(index)
+            # decrement i
+            global i
+            #i -= 1
+            # loop through listbox items and renumber them 
+            for i in range(listbox.size()):
+                listbox.delete(i)
+                listbox.insert(i, str(i+1) + '.' + str(lista[i][0]) + " " + str(lista[i][1]) + " " + lista[i][2])
+                i += 2
+        else:
+            messagebox.showinfo("Warning", "You didn't select an item!")
+    # write help function that will display help message to user 
+    def help():
+        messagebox.showinfo("Help", "Welcome to the help section!\n\nTo record mouse clicks, press the L for left click, R for right click and D for double click.\
+        \n\nTo type text press entry button, write text and press add button to add text to listbox.\
+        \n\nTo record hotkeys like ctrl+s or alt+f4, choose option from hotkey button, press entry button, type a letter and press add button to add choosen combination to listbox.\
+        \n\nTo save your list, press the save button.\n\nTo load a list, press the load button.\
+        \n\nTo edit an item, double click on it.\n\nTo remove an item, press the remove button.\
+        \n\nTo clear the list, press the clear button.\n\nTo start the program, press the start button.\
+        \n\nTo exit the program, press the exit button.")
+    # add file menu to the menu bar with commands
+    file_menu = Menu(root, tearoff=0)
+    root.config(menu=file_menu)
+    file_menu.add_command(label="Start", command=start)
+    file_menu.add_command(label="Save", command=save)
+    file_menu.add_command(label="Load", command=load)
+    file_menu_new = Menu(file_menu, tearoff=0)
+    file_menu_new.add_radiobutton(label="Ctrl", variable=hotkey, value='ctrl')
+    file_menu_new.add_radiobutton(label="Alt", variable=hotkey, value='alt')
+    file_menu_new.add_radiobutton(label="Shift", variable=hotkey, value='shift')
+    file_menu_new.add_radiobutton(label="Win", variable=hotkey, value='win')
+    file_menu.add_cascade(label="Hotkey", menu=file_menu_new)
+    file_menu.add_command(label="Entry", command=printer)
+    file_menu.add_command(label="Add", command=record_keyboard_input)
+    file_menu.add_command(label="Clear", command=clear)
+    file_menu.add_command(label="Remove", command=delete)
+    file_menu.add_command(label="Help", command=help)
     # make a listbox
-    listbox = tk.Listbox(root, width=50, height=20)
+    listbox = tk.Listbox(root, width=59, height=20)
     listbox.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
     # edit items in listbox with double click on item in listbox 
     listbox.bind('<Double-Button-1>', edit)   
-    # start record_mouse_position() function when key is pressed
-    #root.bind('<space>', record_mouse_position)
     root.bind('<l>', L)
     root.bind('<r>', R)
     root.bind('<d>', D)
